3e65ab791ed1eb8bf4c7983d76079eac3f53acba
deviniverson <iverson.devin7@gmail.com>
2022-11-03 22:10:29 -0700

    challenge37 starter code

---
+++ Class37:CookieCapture/CookieCapture.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# File Name: CookieCapture.py
+# Author: Devin Iverson
+# Date: 11/03/2022
+# Purpose:
+# The below Python script shows one possible method to return the cookie from a site that supports cookies.
+
+import requests
+
+# targetsite = input("Enter target site:") # Uncomment this to accept user input target site
+targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
+response = requests.get(targetsite)
+cookie = response.cookies
+
+def bringforthcookiemonster(): # Because why not!
+    print('''
+
+              .---. .---.
+             :     : o   :    me want cookie!
+         _..-:   o :     :-.._    /
+     .-''  '  `---' `---' "   ``-.
+   .'   "   '  "  .    "  . '  "  `.
+  :   '.---.,,.,...,.,.,.,..---.  ' ;
+  `. " `.                     .' " .'
+   `.  '`.                   .' ' .'
+    `.    `-._           _.-' "  .'  .----.
+      `. "    '"--...--"'  . ' .'  .'  o   `.
+
+        ''')
+
+bringforthcookiemonster()
+print("Target site is " + targetsite)
+print(cookie)
+
+# Add here some code to make this script perform the following:
+# - Send the cookie back to the site and receive a HTTP response
+# - Generate a .html file to capture the contents of the HTTP response
+# - Open it with Firefox
+#
+# Stretch Goal
+# - Give Cookie Monster hands
